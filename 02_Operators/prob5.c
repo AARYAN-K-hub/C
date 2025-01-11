942b3c781d418494de77f24a0d3ad2069cfa1dbd
Aaryan Katoch <aaryankatoch41@gmail.com>
2025-01-11 14:55:32 +0530

    Add files via upload

---
+++ 02_Operators/prob5.c
@@ -0,0 +1,11 @@
+/*  3.0 + 1 will be:
+    a. Integer.
+    b. Floating point number.
+    c. Character. */
+#include<stdio.h>
+
+int main(){
+    float a=3.0+1;
+    printf("the value of a is %f",a);
+    return 0;
+}
